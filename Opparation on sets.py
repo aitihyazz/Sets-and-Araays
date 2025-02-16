02043a361b97ec77e9bc655b1f82e12acfa751f7
aitihyazz <Your GitHub Email>
2025-02-16 22:25:27 +0600

    2

---
+++ Opparation on sets.py
@@ -0,0 +1,13 @@
+set1 ={1,2,3,4,5}
+print(set1)
+set1={"hello",1.9,True,(1,2,3)}
+print(set1)
+set1={1,1,2,3,3,4}
+print(set1)
+set1=set([1,2,3])
+print(set1)
+set2=set([0,1,2,3,4])
+print(set2)
+set2.pop()
+print("After removing the first element of the set")
+print(set2,"\n")
